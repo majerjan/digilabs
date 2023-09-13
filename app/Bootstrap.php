60ed934fe1c3a7c7b68bf294376fb463f7674c3a
Jan Majer <>
2023-09-13 10:59:47 +0200

    add pathHelper and add debug mode from config

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -4,23 +4,33 @@ declare(strict_types=1);
 
 namespace App;
 
+use App\Helpers\PathHelper;
 use Nette\Bootstrap\Configurator;
+use Nette\DI\Config\Loader;
 
 
 class Bootstrap
 {
 	public static function boot(): Configurator
 	{
+        $loader = new Loader();
+        $appDir = PathHelper::getRoot();
+        $configDir = PathHelper::getConfig();
+        $content = $loader->load(PathHelper::concatPath([$configDir, 'local.neon']));
 		$configurator = new Configurator;
-		$appDir = dirname(__DIR__);
 
-        $configurator->setDebugMode(true);
-		$configurator->enableTracy($appDir . '/log');
+        if (
+            array_key_exists('parameters', $content) &&
+            array_key_exists('debugMode', $content['parameters'])
+        ) {
+            $configurator->setDebugMode($content['parameters']['debugMode']);
+        }
 
-		$configurator->setTempDirectory($appDir . '/temp');
+		$configurator->enableTracy(PathHelper::concatPath([$appDir, 'log']));
+		$configurator->setTempDirectory(PathHelper::concatPath([$appDir, 'temp']));
 
-		$configurator->addConfig($appDir . '/config/common.neon');
-		$configurator->addConfig($appDir . '/config/services.neon');
+		$configurator->addConfig(PathHelper::concatPath([$configDir, 'common.neon']));
+		$configurator->addConfig(PathHelper::concatPath([$configDir, 'services.neon']));
 
 		return $configurator;
 	}
